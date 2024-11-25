5e44d311528eab87d9ba2f6256e23120c55abdff
Daniil Rybalko <pslpslpslpsl@yandex.ru>
2024-11-26 02:27:54 +0300

    wip: codepad widget

--- Source/disco_elysium_ripoff/Private/CodepadDeviceUI.cpp
+++ Source/disco_elysium_ripoff/Private/CodepadDeviceUI.cpp
@@ -3,3 +3,12 @@
 
 #include "CodepadDeviceUI.h"
 
+FReply UCodepadDeviceUI::NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) {
+
+	Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+	if (InKeyEvent.GetKey() == EKeys::Escape) {
+		this->RemoveFromViewport();
+		return FReply::Handled();
+	}
+	return FReply::Handled();
+}
--- Source/disco_elysium_ripoff/Public/CodepadDeviceUI.h
+++ Source/disco_elysium_ripoff/Public/CodepadDeviceUI.h
@@ -13,7 +13,21 @@ UCLASS()
 class DISCO_ELYSIUM_RIPOFF_API UCodepadDeviceUI : public UBaseDeviceUI
 {
 	GENERATED_BODY()
+
+	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 	
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
 	class UButton* CloseButton;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* InputBox;
+
+	UPROPERTY(EditAnywhere, Category = InputButtons)
+	TArray<class UButton*> InteractionButtons; //???
+
+	UPROPERTY(EditAnywhere, Category = InputButtons)
+	TMap<class UButton*, FKey> ButtonMappings; //???
+
+	UPROPERTY(EditAnywhere, Category = InputButtons)
+	TMap<FKey, FString> ValueMappings;
 };
