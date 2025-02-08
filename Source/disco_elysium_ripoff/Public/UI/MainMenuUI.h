9abf6603b901ec1d6f947d388411e831d352cc4d
Daniil Rybalko <pslpslpslpsl@yandex.ru>
2025-02-08 20:16:09 +0300

    wip: main menu

---
+++ Source/disco_elysium_ripoff/Public/UI/MainMenuUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DISCO_ELYSIUM_RIPOFF_API UMainMenuUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
