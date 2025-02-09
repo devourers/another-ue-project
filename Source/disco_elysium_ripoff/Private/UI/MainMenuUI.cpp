aa67175422f4302b06788b84b5bc7db7317d4659
Daniil Rybalko <pslpslpslpsl@yandex.ru>
2025-02-09 20:52:07 +0300

    wip: boilerplate for main menu ui

--- Source/disco_elysium_ripoff/Private/UI/MainMenuUI.cpp
+++ Source/disco_elysium_ripoff/Private/UI/MainMenuUI.cpp
@@ -2,4 +2,36 @@
 
 
 #include "UI/MainMenuUI.h"
+#include "Components/Button.h"
 
+void UMainMenuUI::NativeConstruct(){
+	ContinueButton->OnClicked.AddUniqueDynamic(this, &UMainMenuUI::OnContinueButtonClicked);
+	NewGameButton->OnClicked.AddUniqueDynamic(this, &UMainMenuUI::OnNewGameButtonClicked);
+	LoadGameButton->OnClicked.AddUniqueDynamic(this, &UMainMenuUI::OnLoadGameButtonPressed);
+	OptionsButton->OnClicked.AddUniqueDynamic(this, &UMainMenuUI::OnOptionsButtonPressed);
+	QuitButton->OnClicked.AddUniqueDynamic(this, &UMainMenuUI::OnQuitButtonPressed);
+}
+
+FReply UMainMenuUI::NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent){
+	return FReply::Handled();
+}
+
+void UMainMenuUI::OnContinueButtonClicked() {
+
+}
+
+void UMainMenuUI::OnNewGameButtonClicked() {
+
+}
+
+void UMainMenuUI::OnLoadGameButtonPressed() {
+
+}
+
+void UMainMenuUI::OnOptionsButtonPressed() {
+
+}
+
+void UMainMenuUI::OnQuitButtonPressed() {
+
+}
--- Source/disco_elysium_ripoff/Public/UI/MainMenuUI.h
+++ Source/disco_elysium_ripoff/Public/UI/MainMenuUI.h
@@ -13,5 +13,45 @@ UCLASS()
 class DISCO_ELYSIUM_RIPOFF_API UMainMenuUI : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+
+protected:
+	UFUNCTION()
+	void OnContinueButtonClicked();
+
+	UFUNCTION()
+	void OnNewGameButtonClicked();
+
+	UFUNCTION()
+	void OnLoadGameButtonPressed();
+
+	UFUNCTION()
+	void OnOptionsButtonPressed();
+
+	UFUNCTION()
+	void OnQuitButtonPressed();
+
+protected:
 	
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* ContinueButton;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* NewGameButton;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* LoadGameButton;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* OptionsButton;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UButton* QuitButton;
+
+private:
+
 };
